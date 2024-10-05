529e41fe57450ad842f603ac09e5d8d917b102ea
JIWONKIM <4526as@naver.com>
2024-10-05 11:14:16 +0900

    last commit

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -3,36 +3,46 @@ const userController = {};
 
 // 유저 정보를 저장하는 함수
 userController.saveUser = async (userName, sid) => {
-    // 이미 있는 유저인지 확인
-    let user = await User.findOne({ name: userName }); // 이름이 userName인 유저 찾기
+    try {
+        // 이미 있는 유저인지 확인
+        let user = await User.findOne({ name: userName });
 
-    // 없다면 새로 유저 정보 만들기
-    if (!user) {
-        user = new User({
-            name: userName,
-            token: sid,
-            online: true,
-        });
+        // 없다면 새로 유저 정보 만들기
+        if (!user) {
+            user = new User({
+                name: userName,
+                token: sid,
+                online: true,
+            });
+        }
+
+        // 이미 있는 유저라면 연결 정보(token 값)만 업데이트
+        user.token = sid;
+        user.online = true;
+
+        await user.save(); // 유저 정보 저장
+
+        return user;
+    } catch (error) {
+        console.error("Error saving user:", error);
+        throw new Error("Error saving user");
     }
-
-    // 이미 있는 유저라면 연결 정보(token 값)만 업데이트
-    user.token = sid;
-    user.online = true;
-
-    await user.save(); // 유저 정보 저장
-
-    return user;
 };
 
 // 유저를 찾는 함수
 userController.checkUser = async (sid) => {
-    const user = await User.findOne({ token: sid }); // token이 sid인 유저 찾기
+    try {
+        const user = await User.findOne({ token: sid }); // token이 sid인 유저 찾기
 
-    if (!user) {
-        throw new Error("User not found");
+        if (!user) {
+            return null; // 유저를 찾지 못하면 null 반환
+        }
+
+        return user;
+    } catch (error) {
+        console.error("Error checking user:", error);
+        throw new Error("Error checking user");
     }
-
-    return user;
 };
 
 module.exports = userController;
--- utils/io.js
+++ utils/io.js
@@ -7,42 +7,38 @@ module.exports = function(io) {
     io.on("connection", async (socket) => { // 연결할 사람 있는지 듣기
         console.log("client is connected", socket.id);
 
-        socket.on("login",async(userName,cb)=>{
-            // 유저 정보를 저장하는 함수
-            try{
-                const user = await userController.saveUser(userName,socket.id);
-                cb({ok:true,date:user}) // 콜백함수
-            }catch(error)
-            {
-                cb({ok:false,error:error.message});
+        socket.on("login", async (userName, cb) => {
+            console.log("User name received:", userName); // 사용자 이름 확인
+            try {
+                const user = await userController.saveUser(userName, socket.id);
+                cb({ ok: true, data: user }); // 수정된 부분
+                const welcomeMessage = { // 유저가 처음 들어왔을떄
+                    chat: `${user.name} has joined the room`, // 템플릿 리터럴 수정
+                    user: { id: null, name: "system" },
+                };
+                io.emit("message",welcomeMessage); // 모두에게 welcomeMessage 보내기
+            } catch (error) {
+                cb({ ok: false, error: error.message });
             }
         }); //로그인으로 말했을때 이 함수를 실행시키겠다.
 
-        socket.on("sendMessage",async(message,cb)=>{
-            try{
-            //유저찾기 socket id로
-            const user = await userController.checkUser(socket.id);
-            //메세지 저장(유저 )
-            const newMessage = await chatController.saveChat(message,user);
-            io.emit("message",newMessage); //(서버가 다른 클라이언트에게 말하는 작업)다른 클라이언트들에게 알려주는 작업해야함
-            cb({ok:true})
+        socket.on("sendMessage", async (message, cb) => {
+            try {
+                // 유저 찾기 socket id로
+                const user = await userController.checkUser(socket.id);                
+                // 메세지 저장(유저)
+                const newMessage = await chatController.saveChat(message, user);
+                io.emit("message", newMessage); //(서버가 다른 클라이언트에게 말하는 작업) 다른 클라이언트들에게 알려주는 작업
+                cb({ ok: true });
+            } catch (error) {
+                cb({ ok: false, error: error.message }); // 수정된 부분
             }
-            catch(error)
-            {
-                cb({ok:true, error: error.message});
-            }
-
         }); //sendMessage라는 말을 들었을때 실행되는 함수(메세지 내용과 콜백함수가 반환됨)
 
         // 연결 오류 로그 추가
         socket.on("error", (error) => {
             console.error("Socket error:", error);
         });
-
-        // 다른 사용자에게 메시지를 전송하는 예제 (필요한 경우)
-        // socket.on("sendMessage", (message) => {
-        //     io.emit("receiveMessage", message);
-        // });
     });
 
     // 서버에 연결 실패 시 오류 처리
